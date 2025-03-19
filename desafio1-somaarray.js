5b71b8402de2bb495958f989bc34f71b96d422ca
Ana Lu <ana.biancon@sou.unijui.edu.br>
2025-03-19 10:21:29 -0300

    refactor: substitui soma com variável por reduce() no desafio1-somaarray.js

--- desafio1-somaarray.js
+++ desafio1-somaarray.js
@@ -3,16 +3,16 @@ console.log('\nSoma de todos os elementos de um array de números\n');
 // Função que verifica se há valores inválidos e soma todos os números do array:
 function somaArray(valores) {
     let numeros = valores.split(','); // Os valores foram separados por vírgula. 
-    let soma = 0;
+    let valoresValidos = [];
 
     // Verificar se é NaN; é necessário tirar os espaços do início e fim pois isNaN('') = false ('' é tratado como 0).
     for (let i of numeros) {
         if (i.trim() === '' || isNaN(i)) { 
             return 'Erro: você digitou um ou mais valores inválidos!'
         }
-        soma += Number(i); 
+        valoresValidos.push(Number(i));
     }
-    return soma;
+    return valoresValidos.reduce((soma, num) => soma + num, 0); // Soma dos números válidos.
 }
 
 // Configuração do input:
